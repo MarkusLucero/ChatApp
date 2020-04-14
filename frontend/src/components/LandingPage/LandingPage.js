401169f56e6de9341bc4387e68ecaaf16442d7b0
Travis Persson <travispersson93@gmail.com>
2020-04-14 17:37:08 +0200

    Set up grid layout of app

---
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../../assets/main.css"
+
+/**
+ * LandingPage holds the layout design grid of the app.
+ * 
+ * @returns a div containing the SideDisplayList, InfoDisplayList and FocusedView components 
+ */
+const LandingPage = () => {
+  return (
+      /**
+       *  The styling is made up of Tailwind css classes 
+       *  To change the width of components inside the div search for grid-cols-custom in tailwind.js and change corresponding attr.
+       */
+    <div className="grid grid-cols-custom h-screen">
+      {/* TODO These divs will be replaced by Side display list, info display list and focused view components respectively hence the id:s*/}
+      
+      <div id="side-display-list" className="bg-blue-200"><button></button></div>
+      <div id="info-display-list" className="bg-blue-400"></div>
+      <div id="focused-view" className="bg-blue-600"></div>
+    </div>
+  );
+};
+
+export default LandingPage;
