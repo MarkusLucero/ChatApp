cb6dd9d24f2c05ed64754f30addc60afeed148e7
Travis Persson <travispersson93@gmail.com>
2020-04-15 22:54:51 +0200

    some stylings

---
+++ frontend/src/components/Container/Container.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LandingPage from "../LandingPage/LandingPage"
+
+
+const Container = () => {
+
+    return (
+        <div>
+           <LandingPage/> 
+        </div>
+    )
+}
+
+export default Container;
