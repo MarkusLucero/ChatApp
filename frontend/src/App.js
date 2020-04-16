cdc55b921e2df14c1c29d93006f14029fdaafbb5
MarkAnnala <mark.annala.5653@student.uu.se>
2020-04-16 15:23:31 +0200

    merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "./assets/main.css";
 import Container from "./components/Container/Container";
 
 const App = () => {
+
+  const [isLoading, setIsLoading] = useState(true); 
+  const [message, setMessage] = useState("Hejsan"); 
+
+  var ws = new WebSocket("ws://localhost:8080/websocket");
+  ws.onopen= ()=>{console.log("connection established!"); setIsLoading(false)};
+
+  ws.onmessage = (data) =>{console.log(data.data)};
+    
+  const sendMessage = () => {
+    if(!isLoading){
+    ws.send(message);
+    }
+  };
+
+
   return (
     <div className="App">
       <Container />
+      <button onClick ={sendMessage}className="bg-black text-white">Send</button>
     </div>
   );
 };
