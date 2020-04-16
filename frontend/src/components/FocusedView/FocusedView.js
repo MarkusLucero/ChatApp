6ab008efee8914c51bdb04c5babb4d7c354cdf35
Travis Persson <travispersson93@gmail.com>
2020-04-16 13:04:18 +0200

    added comp comments

--- frontend/src/components/FocusedView/FocusedView.js
+++ frontend/src/components/FocusedView/FocusedView.js
@@ -5,8 +5,6 @@ import ChatContainer from "../ChatContainer/ChatContainer";
 /**
  * FocusedView is the component that makes sure to route to specific component that is focused on
  * TODO Actual routing
- * TODO Actual routing
- * TODO Actual routing
  * @returns the appropriate component that should be displayed
  */
 const FocusedView = () => {
