66030d67167f36abf50d2d59101c211ba93326d8
Markus Lucero <markuslucero@hotmail.com>
2020-04-22 17:09:09 +0200

    addded notifcations for register

--- frontend/src/reducers/loginReducer.js
+++ frontend/src/reducers/loginReducer.js
@@ -17,16 +17,12 @@ const loginState = { loginSuccess: false, username: null, loginResponse : null }
 const loginReducer = (state = loginState, action) => {
   switch (action.type) {
     case "SUCCESS": {
-        /*TODO: Add access token to the action payload and add it to localStorage from here ?   */
       return {...state, loginSuccess: true, username : action.payload.username, loginResponse: action.payload.response};
     }
     case "FAILURE": {
         /* Failure simply returns loginState as false */
       return state;
     }
-
-    /* TODO case for register? */
-
     default:
       return state;
   }
