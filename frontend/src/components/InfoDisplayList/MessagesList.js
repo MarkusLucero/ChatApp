1af726d7f433e1cd82bfe513499149f34d73b50a
MarkAnnala <mark.annala.5653@student.uu.se>
2020-04-23 20:37:51 +0200

    Finished with add chat for now

--- frontend/src/components/InfoDisplayList/MessagesList.js
+++ frontend/src/components/InfoDisplayList/MessagesList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import add_chat from "../../img/add_chat.png";
 import StartChat from "./StartChat";
 
@@ -6,6 +6,7 @@ import StartChat from "./StartChat";
  * contains the list of available direct messages that we can chat in
  *
  * @param {function} callback function used to get the target of what we are clicking on
+ * @property the username of the logged in user
  * returns a div containing all direct messages
  */
 const MessagesList = ({ handleFocusedChat, username }) => {
@@ -14,15 +15,38 @@ const MessagesList = ({ handleFocusedChat, username }) => {
   /* Determens if we're showing the add chat modal */
   const [showAddChat, setShowAddChat] = useState(false);
 
+  /*On mount, add event listerner for click outside ref addChatModal*/ 
+  useEffect(()=> {
+   document.addEventListener('mousedown', handleClick);
+   return () =>{
+     document.removeEventListener('mousedown', handleClick);
+   }
+  }, []);
+
+  /*Handle click outside addChatModal */
+  const handleClick = e => {
+    if (addChatModal.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    else{
+      setShowAddChat(false);
+    }
+  };
+  /*Keep track of the modal */
+  const addChatModal = useRef();
+
+
+
   return (
     <div className="flex flex-col ml-2">
       <div
         id="messagesList"
         className="mt-5 text-white text-xl h-auto border-solid border-b-2 border-gray-700 flex flex-row justify-between">
         Direct Messages
-        <img onClick ={() => {setShowAddChat(true)}} src={add_chat} alt="" className="h-6 w-6"/>
+        <img onClick ={() => {setShowAddChat(true)}} src={add_chat} alt="Create a new chat!" className="h-6 w-6"/>
       </div>
-      {showAddChat ? <StartChat username={username} friends ={chats}/> : null}
+      <div ref ={addChatModal}>{showAddChat ? <StartChat setShowAddChat = {setShowAddChat} username={username} friends ={chats}/> : null}</div>
       <div className="flex flex-col">
         {chats.map((chat, index) => {
           return (
--- frontend/src/components/InfoDisplayList/StartChat.js
+++ frontend/src/components/InfoDisplayList/StartChat.js
@@ -2,13 +2,20 @@ import React, {useState} from 'react';
 import * as actions from "../../actions/actions";
 import { useDispatch } from "react-redux";
 
-const StartChat = ({friends, username}) => {
+/**
+ * StartChat is the component which is seen when pressing the '+' icon next to active chats
+ * @property friends, all friends of the logged in user
+ * @property username, the logged in user's username
+ * @param {function} callback is used to show/hide the StartChat modal
+ * @returns A div where user can choose which friend he want's to chat with
+ */
+const StartChat = ({friends, username, setShowAddChat}) => {
       /* useDispatch from dispatch function from store */
     const dispatch = useDispatch();
 
     /*local state for which friend to start chat with     */
-    const [chosen, setChosen] = useState([]);
-
+    const [chosen, setChosen] = useState([username]);
+    /* Used when clicking on a checkbox */
     const handleCheck = (e) =>{
         e.persist();
         if(e.target.checked)
@@ -17,26 +24,35 @@ const StartChat = ({friends, username}) => {
         }
         else
         {
+            /*Friend was clicked, this removes that friend*/
             const prev = chosen.filter(friend => friend != e.target.value); 
             setChosen(prev);
         }
     };
 
     const startChat= () => {
-        const data = {chat_name: username, from: username, members: chosen};
-        dispatch(actions.startChat(data));
-    };
+        //only username in state, noone else chosen
+        if(chosen.length == 1){
+            setShowAddChat(false);
+        }
+        else{
+            const data = {chat_name: username, from: username, members: chosen};
+            dispatch(actions.startChat(data));
+            setShowAddChat(false);
+        }
 
+    };
+    
     return (
-        <div style={{left: "24rem"}}className=" w-56 z-40 bg-white rounded mt-2 bg-gray-700 absolute p-2" >
-            <div className="border-solid border-b border-black">
+        <div style={{left: "24rem", top: "16rem"}} className=" w-56 z-40 rounded mt-2 bg-gray-700 absolute p-2  w-auto" >
+            <div className="border-solid border-b border-black mb-2 pr-3" >
             Choose friends to add:
             </div>
             {friends.map((friend, index) => {
 
                 return <div key={index} className="flex flex-row justify-between">
                              <div className="">{friend}</div>
-                             <input onChange ={handleCheck} type="checkbox" value ={friend}/>
+                             <input onChange ={handleCheck} type="checkbox" className=" "value ={friend}/>
                         </div>
             })} 
             <button className="rounded w-full bg-blue-600 mt-4" onClick={startChat}>Create chat</button>
