26afa4c4392d7b164135889dc374d5fcfc7cd2de
Travis Persson <travispersson93@gmail.com>
2020-04-23 21:36:08 +0200

    todo since it was copy

--- frontend/src/reducers/registerReducer.js
+++ frontend/src/reducers/registerReducer.js
@@ -3,24 +3,21 @@ const registerState = {}
 /**
  * registerReducer handles actions of register
  * 
- * @param {object} state
- * @property {bool} loginSuccess true if login was successfull, otherwise false
- * @property {string} username holds the username that we successfully logged in with
- * @property {string} loginResponse holds the response from the http request
- * @param {object} action 
- * @property {string} type what type of action it is
- * @property {object} payload contains nescessary data for that particular action type
+ * @param {object} state TODO
+ * @param {object} action TODO
  * 
  * @returns updated state
  */
 const registerReducer = (state = registerState, action) => {
   switch (action.type) {
-    case "SUCCESS": {
-        /*TODO: Add access token to the action payload and add it to localStorage from here ?   */
-      return {...state, loginSuccess: true, username : action.payload.username, loginResponse: action.payload.response};
+    case "SUCCESS": { //TODO this is same as login right now... not good
+      
+      //TODO
+      return state;
     }
     case "FAILURE": {
         /* Failure simply returns loginState as false */
+      //TODO
       return state;
     }
 
