89f962a4a61e0258d650604f2221f2c7c09d207d
Alex Kangas <alex.kangas.5644@student.uu.se>
2020-05-05 15:19:25 +0200

    messages object parses correctly

--- backend/web_api/src/chat_server.erl
+++ backend/web_api/src/chat_server.erl
@@ -190,9 +190,10 @@ loop(Connection_map) ->
                 true ->
                     io:format("About to encode ~p~n",[DMs]),
                     %% DMs === [{Chat_ID, Chat_Name, [{Sender,  Message}]}]
+		    %{"messages", lists:map(fun({Src, Msg}) -> mochijson:encode({struct, [{"message", Msg}, {"username", Src}]}) end, Messages)}
                     List_of_DMs = [{struct, [{"chatName", Chat_Name}, 
-					     {"chatID", Chat_ID}, 
-					     {"messages", lists:map(fun({Src, Msg}) -> mochijson:encode({struct, [{"message", Msg}, {"username", Src}]}) end, Messages)}
+					     {"chatID", Chat_ID},
+					     {"messages", {array, [{struct, [{"message", Msg}, {"username", Src}]} || {Src, Msg} <- Messages]}}
 					    ]} || {Chat_ID, Chat_Name, Messages} = _MsgS <- DMs],
                     io:format("MADE IT PAST LIST OF DMs: ~p~w~n~s~n", [List_of_DMs, length(List_of_DMs), FriendList]),
                     JSON_Message = mochijson:encode(
