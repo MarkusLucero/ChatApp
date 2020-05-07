3be3c416a2ece0866dcab1c55d59c93e857ee96d
Travis Persson <travispersson93@gmail.com>
2020-05-07 08:51:29 +0200

    Logout, reset of state and redirection implemented

--- frontend/src/actions/actions.js
+++ frontend/src/actions/actions.js
@@ -15,6 +15,8 @@ const FAILURE = "FAILURE";
 const SENDMESSAGE = "SENDMESSAGE";
 const ADDFRIEND = "ADDFRIEND";
 const CHAT_REQUEST = "CHAT_REQUEST";
+const LOGOUT = "LOGOUT";
+const RESET = "RESET";
 
 export function sendMessage(data) {
   return {
@@ -25,13 +27,12 @@ export function sendMessage(data) {
       user_id: data.username,
       message: data.message,
       timestamp: null,
-    }
+    },
   };
 }
 export function addFriend(data) {
-  return{   type: ADDFRIEND, payload:{ username: data.username }}
-};
-
+  return { type: ADDFRIEND, payload: { username: data.username } };
+}
 
 export function connect() {
   return { type: CONNECT, payload: null };
@@ -39,6 +40,10 @@ export function connect() {
 
 export function disconnected(data) {
   return { type: DISCONNECTED, payload: data };
+}
+
+export function logOut(data) {
+  return { type: LOGOUT };
 }
 
 export function login({ values }) {
@@ -65,22 +70,22 @@ export function register(values) {
   };
 }
 
-export function startChat(values){
+export function startChat(values) {
   console.log(values);
   return {
     type: CHAT_REQUEST,
     payload: {
       action: "chat_request",
       chat_name: values.chatName,
-      from: values.from, 
+      from: values.from,
       members: values.members,
     },
   };
 }
-export function loginSuccess(data){
-  return { type: SUCCESS, payload: data}
+export function loginSuccess(data) {
+  return { type: SUCCESS, payload: data };
 }
-  
+
 export function loginFailure(data) {
   return { type: FAILURE, payload: data };
 }
@@ -93,3 +98,6 @@ export function setServer(server) {
   return { type: SET, payload: server };
 }
 
+export function resetLoginState() {
+  return { type: RESET };
+}
