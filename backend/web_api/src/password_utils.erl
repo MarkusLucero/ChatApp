7c8aeb19f2c380ae11d866c22eb04ca6f6550045
Per Alonso <Per.Alonso.3561@student.uu.se>
2020-05-14 15:54:20 +0200

    Documented and added regression tests to password_utils

--- backend/web_api/src/password_utils.erl
+++ backend/web_api/src/password_utils.erl
@@ -22,3 +22,23 @@ hash_password(Password) ->
     Hash = crypto:hash(sha3_256, Password),
     io:format("Hash: ~p~n", [Hash]),
     lists:flatten([integer_to_list(X, 16) || <<X>> <= Hash]).
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+%% TESTS START HERE %%
+%%%%%%%%%%%%%%%%%%%%%%
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+%% Just checks the length of the magic tokens (REGRESSION TEST)
+magic_token_test() ->
+    [?_assertEqual(32, lists:length(?MODULE:get_magic_token())) || _ <- lists:seq(1, 10000)].
+
+%% This one just exists to make sure noone messes up and uses the wrong hashing algorithm (REGRESSION TEST)
+hash_test() ->
+    "73A83D6DAEE2AF7E94211D4CA4978A9FA13F46CF1B7DEFD7C2743EDFE24E7C3" = password_utils:hash_password("adrenaline"),
+    "4876E8F21DD719FA3D3EB88EB6A936BD5D35287CBFEE546BE9CB86C6BF1031" = password_utils:hash_password("our security isnt very good"),
+    "FFFA816B58824F3F4C872ECAC1D18F9EB7F2123470D437EAFE3FF75E1C3923" = password_utils:hash_password("secure_password123").
+-endif.
