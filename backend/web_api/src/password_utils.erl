20644702fd4907985af05368e696e90ebbc7a3c1
Per Alonso <Per.Alonso.3561@student.uu.se>
2020-05-14 15:27:05 +0200

    Added password utils documentation

--- backend/web_api/src/password_utils.erl
+++ backend/web_api/src/password_utils.erl
@@ -1,13 +1,23 @@
+%% @doc This module provides some helpful utilities for passwords and hashing
+
 -module(password_utils).
 
 -export([get_magic_token/0]).
 -export([hash_password/1]).
 
-%%32 chars should be enough
-%%33 126
+-spec get_magic_token() -> list().
+%% @doc Generates a magic token for a user to authenticate their connections
+%% @returns A 32 character long random magic token (printable ASCII)
+%% 32 chars should be enough, though no actual entropy calculations have been done
+%% It's also not entirely clear whether this actually uses cryptographically secure random
 get_magic_token() ->
     [33 + rand:uniform(93) || _ <- lists:seq(1, 32)].
 
+
+-spec hash_password(list()) -> list().
+%% @doc Hashes a password with 256 bit sha3 (NEITHER WITH SALT NOR PEPPER)
+%% @param Password The password to hash
+%% @returns A 256 bit hashed password in string format
 hash_password(Password) ->
     Hash = crypto:hash(sha3_256, Password),
     io:format("Hash: ~p~n", [Hash]),
