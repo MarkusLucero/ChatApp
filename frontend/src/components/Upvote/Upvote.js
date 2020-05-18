096aec0f7776b6593c8b98eed668b4a570424970
Markus Lucero <markuslucero@hotmail.com>
2020-05-18 17:20:03 +0200

    Added non working upvote/downvote button component. Only renders no functionality

---
+++ frontend/src/components/Upvote/Upvote.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Up from "../../img/uploading.svg";
+import Down from "../../img/multimedia-option.svg";
+
+const Upvote = () => {
+  return (
+    <div>
+      <img alt="" src={Up} className="h-4 w-4"></img>
+      <div className="ml-1">0</div>
+      <img alt="" src={Down} className="h-4 w-4"></img>
+    </div>
+  );
+};
+
+export default Upvote;
